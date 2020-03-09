9a893661bbd8a9125a7eb608901b1715aff9f7c3
Anna <anna_agabekyan_anna@mail.ru>
2020-03-09 20:29:54 +0300

    feat:add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+  for(let i = 0; i < str.length; i++){
+  let elemConfig = bracketsConfig.find(elem => elem.includes(str[i]));
+  
+  if((elemConfig[0] === arr[arr.length - 1]) && elemConfig[1] === str[i]) arr.pop();
+  else arr.push(str[i]);
+  }
+  return !arr.length;
+  }
+
